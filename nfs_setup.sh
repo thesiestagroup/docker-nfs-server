7b1c9e757dbf5d6e59ef20da135527622754d832
gergely.mentsik <gergely.mentsik@thesiestagroup.com>
2017-08-17 11:12:24 +0200

    Added crossmnt option

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -9,7 +9,7 @@ echo "#NFS Exports" > /etc/exports
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash,crossmnt)" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
